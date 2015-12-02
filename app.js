d0465b1a1d73ba74c196b7f8d2e6612d85d1acbe
Huy Tran <hqtran@wpi.edu>
2015-12-02 16:32:40 -0500

    setup basic static page

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+var express = require('express'),
+  app = express(),
+  path = require('path'),
+  port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, './public')));
+
+app.listen(port, function () {
+  console.log('server is running on ' + port);
+});
